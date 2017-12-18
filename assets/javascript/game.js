33107d1b7dd39a7fa4caa635177240824d35f210
Anna P <anna_p@mail.com>
2017-12-17 22:38:02 -0500

    third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,24 @@
-/*
-var link = document.getElementById('getNumber'); 
-link.onclick = getNumber; 
-
-function getNumber() {
-    var minNumber = 1; // le minimum
-    var maxNumber = 100; // le maximum
-    var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // la fonction magique
-    $('#result').html(randomnumber);
-    return false;
-}   
-*/
-
-
-/*
-function targetScore(start, end) {
-    return Array(end - start + 1).fill().map((_, idx) => start + idx)
-  }
-  var result = range(19, 120); // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-  console.log(result);
-*/
-
+//=================================================================
+//                  VARIABLES
 
 var computerTargetScore = Math.floor((Math.random() * 120) + 19);
 console.log("The computer generated target score is: " + computerTargetScore);
 
-
-
+// crystal 1 = Amethyst
 var crystalValue1 = 0;
-//Math.floor((Math.random() * 12) + 1);
-//console.log("The amethyst value is: " +computerTargetScore);
+// crystal 2 = Brown Crystal
 var crystalValue2 = 0;
+// crystal 3 = Colorful Crystal
 var crystalValue3 = 0;
-
+// crystal 4 = Crystal Rocks
+var crystalValue4 = 0;
 
 //===================================================================
 //                  FUNCTIONS
 
 // Function that updates the Target Score...
 function UpdateTargetScore() {
-  //  document.querySelector("points").innerHTML = computerTargetScore;
+//  Print Target Score to UI
   document.querySelector("#points").innerHTML = computerTargetScore;
 }
 UpdateTargetScore();
@@ -46,17 +26,63 @@ UpdateTargetScore();
 //}
 
 //TargetScore(); 
-// This function is run whenever the userclicks on the amethyst crystal; 
+// This function is run the first time a user clicks on the amethyst crystal; 
 // create an onClick event that triggers an on click event.
 
 
-
+// code to generate a random value for the amethyst and to add the value to the counter
 $("#amethyst").on( "click", function() {
     crystalValue1 = Math.floor((Math.random() * 12) + 1);
-
+    counter+=crystalValue1;
+    document.querySelector("#counter").innerHTML = crystalValue1;
+    // below code makes the randomly generated value stick, but you cannot re-click the image
+    //$( "#amethyst" ).unbind( "click" );
 
     console.log( "The value of an amethyst is " + crystalValue1 );
   });
+
+  
+  // code to generate a random value for the brown crystal and to add the value to the counter
+  $("#brown_crystal").on( "click", function() {
+    crystalValue2 = Math.floor((Math.random() * 12) + 1);
+    counter+=crystalValue2;
+    document.querySelector("#counter").innerHTML = crystalValue2;
+    // below code makes the randomly generated value stick, but you cannot re-click the image
+    //$( "#brown_crystal" ).unbind( "click" );
+
+    console.log( "The value of a brown crystal is " + crystalValue2 );
+  });
+
+
+  // code to generate a random value for the colorful crystal and to add the value to the counter
+  $("#colorful_crystal").on( "click", function() {
+    crystalValue3 = Math.floor((Math.random() * 12) + 1);
+    counter+=crystalValue3;
+    document.querySelector("#counter").innerHTML = crystalValue3;
+    // below code makes the randomly generated value stick, but you cannot re-click the image
+    //$( "#colorful_crystal" ).unbind( "click" );
+
+    console.log( "The value of a colorful crystal is " + crystalValue3 );
+  });
+
+  // code to generate a random value for the crystal rocks and to add the value to the counter
+  $("#crystal-rocks").on( "click", function() {
+    crystalValue4 = Math.floor((Math.random() * 12) + 1);
+    counter+=crystalValue4;
+    document.querySelector("#counter").innerHTML = crystalValue4;
+    // below code makes the randomly generated value stick, but you cannot re-click the image
+    //$( "#crystal-rocks" ).unbind( "click" );
+
+    console.log( "The value of a crystal rock is " + crystalValue4 );
+  });
+
+  /*
+  var counter = 0;
+  $(".crystal-image").on("click", function() {
+
+    counter += 10;
+
+    alert("Your new score is: " + counter);
 
 
 /*  $("#amethyst").on("click", function(clickAmethyst) {
